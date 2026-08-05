49911db83ca057f91e36c50ce4ed529ab64a8874
pasquale sorrentino <pasquale.sorrentino.deadco@gmail.com>
2026-08-05 19:10:31 +0100

    dhjhj

--- scriptcopy.py
+++ scriptcopy.py
@@ -3,6 +3,31 @@ from enum import IntEnum
 import asyncio
 from re import split
 
+#
+#mt5.TIMEFRAME_M1    # 1 minuto
+#mt5.TIMEFRAME_M2    # 2 minuti
+#mt5.TIMEFRAME_M3    # 3 minuti
+#mt5.TIMEFRAME_M4    # 4 minuti
+#mt5.TIMEFRAME_M5    # 5 minuti
+#mt5.TIMEFRAME_M6    # 6 minuti
+#mt5.TIMEFRAME_M10   # 10 minuti
+#mt5.TIMEFRAME_M12   # 12 minuti
+#mt5.TIMEFRAME_M15   # 15 minuti
+#mt5.TIMEFRAME_M20   # 20 minuti
+#mt5.TIMEFRAME_M30   # 30 minuti
+
+#mt5.TIMEFRAME_H1    # 1 ora
+#mt5.TIMEFRAME_H2    # 2 ore
+#mt5.TIMEFRAME_H3    # 3 ore
+#mt5.TIMEFRAME_H4    # 4 ore
+#mt5.TIMEFRAME_H6    # 6 ore
+#mt5.TIMEFRAME_H8    # 8 ore
+#mt5.TIMEFRAME_H12   # 12 ore
+
+#mt5.TIMEFRAME_D1    # giorno
+#mt5.TIMEFRAME_W1    # settimana
+#mt5.TIMEFRAME_MN1   # mese
+
 
 class ORDER(IntEnum): #ACTIONS
     ACT_INSTANT = mt5.TRADE_ACTION_DEAL #ordine immediato a mercato
@@ -34,7 +59,41 @@ class ORDER(IntEnum): #ACTIONS
 
 #region  ------------------------------------- META TRADER CONNECTION ----------------------------------
 #-----------------------------------------------------------------------------------------------------
+
+async def main():
+    rates = mt5.copy_rates_from_pos(
+        "XAUUSD",          # simbolo
+        mt5.TIMEFRAME_M5,  # timeframe
+        0,                 # da quale candela partire
+        100                # numero candele
+    )
+    #print(rates[-1] ) #candela corrente
+    #for candle in rates:
+        #time          1785961500  → apertura candela
+        #open          4240.01     → prezzo apertura
+        #high          4246.52     → massimo raggiunto
+        #low           4237.27     → minimo raggiunto
+        ##close         4244.90     → chiusura
+        #tick_volume   2216        → numero movimenti prezzo
+        #spread        9           → spread in punti
+        #real_volume   0           → volume reale non disponibile
+        # example(1785960900, 4241.87, 4241.87, 4237.92, 4238.64, 1159, 7, 0)
+
+
+
+        #print(mt5.symbol_info_tick("XAUUSD"))
+        #Tick(time=1785961191, bid=4238.04, ask=4238.31, last=0.0, volume=0, time_msc=1785961191713, flags=1030, volume_real=0.0)
+
+
+
+
+
+
+
+
 if not mt5.initialize():
     print("initialize() failed")
     mt5.shutdown()
 
+elif __name__ == "__main__":
+    asyncio.run(main())
